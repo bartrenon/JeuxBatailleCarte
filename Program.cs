536f787f2fa615e175fcb95e40eb2ecfefba8653
Renon bart <b.renon@mail.com>
2026-03-12 16:50:47 +0100

    Enum Valeur carte créer

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
-﻿public enum couleurs 
+﻿
+public enum couleurs 
 {
     Coeur = 1,
     Carreau,
     Pique,
     Trefle
 } 
+
+public enum Valeurs 
+{
+    As = 1,
+    Deux,
+    Trois,
+    Quatre,
+    Cinq,
+    Six,
+    Sept,
+    huit,
+    Neuf,
+    Dix,
+    Valet,
+    Reine,
+    Roi,
+}
