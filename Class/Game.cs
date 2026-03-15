e2f62a614c4bcd0bb8ed1ed25ee4674734b61a36
Renon bart <b.renon@mail.com>
2026-03-15 09:09:23 +0100

    Création des class game et jouer

---
+++ Class/Game.cs
@@ -0,0 +1,15 @@
+﻿public class Game
+{
+    public Joueur j1 { get; };
+
+    public Joueur j2 { get; };
+
+    public int score1 = 0 { get; };
+
+    public int score2 = 0 { get; };
+
+    public getScore()
+    {
+        Console.WriteLine($"{}: {name}");
+    }
+}
---
+++ Class/Joueur.cs
@@ -0,0 +1,11 @@
+﻿public class Joueur
+{
+    public string name { get; };
+
+    public Carte[26] DeckCarte { get; set; };
+
+    public getJoueur()
+    {
+        Console.WriteLine($"Carte: {name}");
+    }
+}
