02bd34e429817a7e2bdbf645e3a59c2fd29e277b
Renon bart <b.renon@mail.com>
2026-03-16 10:44:26 +0100

    petit changement

--- Program.cs
+++ Program.cs
@@ -28,6 +28,11 @@ while (continuer)
     for (int i = 1; i <= (deck.nbcarte / 2); i++)
     {
         game.gagnerManche(j1.jouerCarte(), j2.jouerCarte());
+
+        Console.clear();
+
+        Console.WriteLine("Appuie sur une touche pour continuer");
+        Console.ReadLine();
     }
 
     game.gagnerMatch();
@@ -38,35 +43,3 @@ while (continuer)
 
 Console.WriteLine("Bonjour voulez vous jouer ? oui/non");
 choix = Console.ReadLine();
-
-while (continuer)
-{
-
-    if(choix == "non") 
-    {
-        continuer = false;
-        break;
-    }
-
-    Console.WriteLine("Entrez le nom de joueur 1");
-    Joueur j1 = new Joueur(Console.ReadLine());
-
-    Console.WriteLine("Entrez le nom de joueur 2");
-    Joueur j2 = new Joueur(Console.ReadLine());
-
-    Deck deck = new Deck(52);
-    deck.melangerDeck();
-    Game game = new Game(j1, j2, deck);
-
-    game.distribuerCarte();
-
-    for (int i = 1; i <= (deck.nbcarte/2) ; i++)
-    {
-        game.gagnerManche(j1.jouerCarte(),j2.jouerCarte());
-    }
-
-    game.gagnerMatch();
-
-    Console.WriteLine("Voulez vous re jouer ? oui/non");
-    choix = Console.ReadLine();
-}
